9925eb960bff6377de361d3be5cff453daddfe94
Ilian Georgiev <zelin11@live.com>
2019-10-16 20:20:45 +0300

    More tests

--- hr_rfid_tests/tests/test_hr_rfid_card.py
+++ hr_rfid_tests/tests/test_hr_rfid_card.py
@@ -64,7 +64,68 @@ class CardTests(common.SavepointCase):
         cls._acc_grs[2].add_doors(cls._doors[2] + cls._doors[3], cls._def_ts)
         cls._acc_grs[3].add_doors(cls._doors[4], cls._other_ts)
 
-    def test_card_creation(self):
+    def test_get_owner_fns(self):
+        env = self.env
+        card = create_card(env, '0000000001', self._employees[0])
+        self.assertEqual(card.get_owner(), self._employees[0])
+        card = create_card(env, '0000000002', self._contacts[0])
+        self.assertEqual(card.get_owner(), self._contacts[0])
+
+    def test_get_potential_access_doors(self):
+        env = self.env
+        card1 = create_card(env, '0000000001', self._employees[0])
+        card2 = create_card(env, '0000000002', self._contacts[2])
+
+        potential_doors = [ (self._doors[0], self._def_ts) ]
+        self.assertCountEqual(card1.get_potential_access_doors(), potential_doors)
+
+        potential_doors = [ (self._doors[2], self._def_ts), (self._doors[3], self._def_ts) ]
+        self.assertCountEqual(card2.get_potential_access_doors(), potential_doors)
+
+        self._contacts[2].add_acc_gr(self._acc_grs[3])
+        potential_doors.append((self._doors[4], self._other_ts))
+        self.assertCountEqual(card2.get_potential_access_doors(), potential_doors)
+
+        potential_doors = [ (self._doors[4], self._other_ts) ]
+        self.assertCountEqual(card2.get_potential_access_doors(self._acc_grs[3]), potential_doors)
+
+        potential_doors = [ ]
+        self.assertCountEqual(card2.get_potential_access_doors(self._acc_grs[0]), potential_doors)
+
+    def test_door_compatible(self):
+        env = self.env
+        card = create_card(env, '0000000001', self._employees[0])
+        door = self._doors[0]
+
+        other_card_type = env.ref('hr_rfid.hr_rfid_card_type_1')
+
+        self.assertTrue(card.door_compatible(door))
+
+        card.card_type = other_card_type
+        self.assertFalse(card.door_compatible(door))
+
+        door.card_type = other_card_type
+        self.assertTrue(card.door_compatible(door))
+
+        card.cloud_card = False
+        self.assertTrue(card.door_compatible(door))
+
+        door.controller_id.external_db = True
+        self.assertTrue(card.door_compatible(door))
+
+        card.cloud_card = True
+        self.assertFalse(card.door_compatible(door))
+
+    def test_card_ready(self):
+        env = self.env
+        card = create_card(env, '0000000001', self._employees[0])
+
+        self.assertTrue(card.card_ready())
+
+        card.card_active = False
+        self.assertFalse(card.card_ready())
+
+    def test_create(self):
         # Check if creating a card with a length different from 10 raises
         with self.assertRaises(exceptions.ValidationError):
             create_card(self.env, '123', self._employees[0])
@@ -88,35 +149,7 @@ class CardTests(common.SavepointCase):
         with self.assertRaises(IntegrityError):
             create_card(self.env, cards[0].number, self._employees[0])
 
-    def test_get_owner_fns(self):
-        env = self.env
-        card = create_card(env, '0000000001', self._employees[0])
-        self.assertEqual(card.get_owner(), self._employees[0])
-        card = create_card(env, '0000000002', self._contacts[0])
-        self.assertEqual(card.get_owner(), self._contacts[0])
-
-    def test_get_potential_access_doors(self):
-        env = self.env
-
-        card = create_card(env, '0000000001', self._employees[0])
-        potential_doors = [ (self._doors[0], self._def_ts) ]
-        self.assertCountEqual(card.get_potential_access_doors(), potential_doors)
-
-        card = create_card(env, '0000000002', self._contacts[2])
-        potential_doors = [ (self._doors[2], self._def_ts), (self._doors[3], self._def_ts) ]
-        self.assertCountEqual(card.get_potential_access_doors(), potential_doors)
-
-        self._contacts[2].add_acc_gr(self._acc_grs[3])
-        potential_doors.append((self._doors[4], self._other_ts))
-        self.assertCountEqual(card.get_potential_access_doors(), potential_doors)
-
-        potential_doors = [ (self._doors[4], self._other_ts) ]
-        self.assertCountEqual(card.get_potential_access_doors(self._acc_grs[3]), potential_doors)
-
-        potential_doors = [ ]
-        self.assertCountEqual(card.get_potential_access_doors(self._acc_grs[0]), potential_doors)
-
-    def test_create_cards_relations(self):
+    def test_create_doors_relations(self):
         env = self.env
         card1n = '0000000001'
         card2n = '0000000002'
@@ -238,6 +271,7 @@ class CardDoorRelTests(common.SavepointCase):
     @classmethod
     def setUpClass(cls):
         super(CardDoorRelTests, cls).setUpClass()
+        cls._env = cls.env['hr.rfid.card.door.rel']
         cls._ws = create_webstacks(cls.env, webstacks=1, controllers=[2, 2, 0x22])
         cls._doors = get_ws_doors(cls._ws)
         cls._acc_grs = create_acc_grs_cnt(cls.env, 4)
@@ -247,6 +281,16 @@ class CardDoorRelTests(common.SavepointCase):
                                           ['Max', 'Cooler Max', 'Jacob', 'Andrej'],
                                           [cls._departments[0], cls._departments[1]])
         cls._contacts = create_contacts(cls.env, ['Greg', 'Cooler Greg', 'Richard'])
+
+        cls._cards = [
+            create_card(cls.env, '0000000001', cls._employees[0]),
+            create_card(cls.env, '0000000002', cls._employees[0]),
+            create_card(cls.env, '0000000003', cls._employees[1]),
+            create_card(cls.env, '0000000004', cls._employees[2]),
+            create_card(cls.env, '0000000005', cls._contacts[0]),
+            create_card(cls.env, '0000000006', cls._contacts[2]),
+            create_card(cls.env, '0000000007', cls._employees[3]),
+        ]
 
         cls._departments[0].hr_rfid_allowed_access_groups = cls._acc_grs[0] + cls._acc_grs[1]
         cls._departments[1].hr_rfid_allowed_access_groups = cls._acc_grs[1] + cls._acc_grs[2] + cls._acc_grs[3]
@@ -259,11 +303,165 @@ class CardDoorRelTests(common.SavepointCase):
         cls._contacts[1].add_acc_gr(cls._acc_grs[1])
         cls._contacts[2].add_acc_gr(cls._acc_grs[2])
 
-        cls._acc_grs[0].add_doors(cls._doors[0])
-        cls._acc_grs[1].add_doors(cls._doors[1])
-        cls._acc_grs[2].add_doors(cls._doors[2])
-        cls._acc_grs[2].add_doors(cls._doors[3])
-        cls._acc_grs[3].add_doors(cls._doors[4])
+        cls._def_ts = cls.env.ref('hr_rfid.hr_rfid_time_schedule_0')
+        cls._other_ts = cls.env.ref('hr_rfid.hr_rfid_time_schedule_1')
 
+        cls._acc_grs[0].add_doors(cls._doors[0], cls._def_ts)
+        cls._acc_grs[1].add_doors(cls._doors[1], cls._def_ts)
+        cls._acc_grs[2].add_doors(cls._doors[2] + cls._doors[3], cls._def_ts)
+        cls._acc_grs[3].add_doors(cls._doors[4], cls._other_ts)
 
+    def test_update_card_rels(self):
+        rel_env = self._env
 
+        rel_env.search([]).unlink()
+        card = self._cards[0]
+        rel_env.update_card_rels(card)
+        ret = rel_env.search([])
+        ret = [ (a.door_id, a.time_schedule_id) for a in ret ]
+        expected = [ (self._doors[0], self._def_ts) ]
+        self.assertCountEqual(ret, expected)
+
+        rel_env.search([]).unlink()
+        card = self._cards[3]
+        rel_env.update_card_rels(card)
+        ret = rel_env.search([])
+        ret = [ (a.door_id, a.time_schedule_id) for a in ret ]
+        expected = [ ]
+        self.assertCountEqual(ret, expected)
+
+        rel_env.search([]).unlink()
+        card = self._cards[5]
+        rel_env.update_card_rels(card)
+        ret = rel_env.search([])
+        ret = [ (a.door_id, a.time_schedule_id) for a in ret ]
+        expected = [
+            (self._doors[2], self._def_ts),
+            (self._doors[3], self._def_ts),
+        ]
+        self.assertCountEqual(ret, expected)
+
+        rel_env.search([]).unlink()
+        card = self._cards[6]
+        rel_env.update_card_rels(card)
+        ret = rel_env.search([])
+        ret = [ (a.door_id, a.time_schedule_id) for a in ret ]
+        expected = [ ]
+        self.assertCountEqual(ret, expected)
+
+        rel_env.search([]).unlink()
+        self._doors[4].controller_id.external_db = False
+        card = self._cards[6]
+        rel_env.update_card_rels(card)
+        ret = rel_env.search([])
+        ret = [ (a.door_id, a.time_schedule_id) for a in ret ]
+        expected = [ (self._doors[4], self._other_ts) ]
+        self.assertCountEqual(ret, expected)
+
+    def test_update_door_rels(self):
+        rel_env = self._env
+
+        rel_env.search([]).unlink()
+        door = self._doors[0]
+        rel_env.update_door_rels(door)
+        ret = rel_env.search([])
+        ret = [ (a.card_id, a.time_schedule_id) for a in ret ]
+        expected = [
+            (self._cards[0], self._def_ts),
+            (self._cards[1], self._def_ts),
+            (self._cards[4], self._def_ts),
+        ]
+        self.assertCountEqual(ret, expected)
+
+        rel_env.search([]).unlink()
+        door = self._doors[4]
+        rel_env.update_door_rels(door)
+        ret = rel_env.search([])
+        ret = [ (a.card_id, a.time_schedule_id) for a in ret ]
+        expected = [ ]
+        self.assertCountEqual(ret, expected)
+
+        rel_env.search([]).unlink()
+        door = self._doors[4]
+        self._cards[6].cloud_card = False
+        rel_env.update_door_rels(door)
+        ret = rel_env.search([])
+        ret = [ (a.card_id, a.time_schedule_id) for a in ret ]
+        expected = [ (self._cards[6], self._other_ts) ]
+        self.assertCountEqual(ret, expected)
+
+        rel_env.search([]).unlink()
+        door = self._doors[5]
+        rel_env.update_door_rels(door)
+        ret = rel_env.search([])
+        ret = [ (a.card_id, a.time_schedule_id) for a in ret ]
+        expected = [ ]
+        self.assertCountEqual(ret, expected)
+
+    def test_check_relevance_slow(self):
+        rel_env = self._env
+        card = self._cards[0]
+        door = self._doors[0]
+
+        def find_card_door_rel(_card, _door):
+            return rel_env.search([ ('card_id', '=', _card.id), ('door_id', '=', _door.id) ])
+
+        rel_env.check_relevance_slow(card, door)
+        ret = find_card_door_rel(card, door)
+        self.assertEqual(len(ret), 1)
+        self.assertEqual(ret.time_schedule_id, self._def_ts)
+
+        rel_env.search([]).unlink()
+        rel_env.check_relevance_slow(card, door)
+        ret = find_card_door_rel(card, door)
+        self.assertEqual(len(ret), 1)
+        self.assertEqual(ret.time_schedule_id, self._def_ts)
+
+        rel_env.search([]).unlink()
+        card.get_owner().remove_acc_gr(self._acc_grs[0])
+        rel_env.check_relevance_slow(card, door)
+        ret = find_card_door_rel(card, door)
+        self.assertEqual(len(ret), 0)
+
+    def test_check_relevance_fast(self):
+        rel_env = self._env
+        card = self._cards[0]
+        door1 = self._doors[0]
+        door2 = self._doors[4]
+
+        def find_card_door_rel(_card, _door):
+            return rel_env.search([ ('card_id', '=', _card.id), ('door_id', '=', _door.id) ])
+
+        rel_env.search([]).unlink()
+        rel_env.check_relevance_fast(card, door1)
+        ret = find_card_door_rel(card, door1)
+        self.assertEqual(len(ret), 1)
+        self.assertEqual(ret.time_schedule_id, self._def_ts)
+
+        rel_env.search([]).unlink()
+        rel_env.check_relevance_fast(card, door2)
+        ret = find_card_door_rel(card, door2)
+        self.assertEqual(len(ret), 0)
+
+        with self.assertRaises(exceptions.ValidationError):
+            rel_env.search([]).unlink()
+            card.cloud_card = False
+            rel_env.check_relevance_fast(card, door2)
+            ret = find_card_door_rel(card, door2)
+            self.assertEqual(len(ret), 1)
+            self.assertEqual(ret.time_schedule_id, self._other_ts)
+
+        rel_env.search([]).unlink()
+        rel_env.check_relevance_fast(card, door2, self._other_ts)
+        ret = find_card_door_rel(card, door2)
+        self.assertEqual(len(ret), 1)
+        self.assertEqual(ret.time_schedule_id, self._other_ts)
+
+        rel_env.search([]).unlink()
+        rel_env.check_relevance_fast(card, door2, self._def_ts)
+        ret = find_card_door_rel(card, door2)
+        self.assertEqual(len(ret), 1)
+        self.assertEqual(ret.time_schedule_id, self._def_ts)
+
+    def test_create_rel(self):
+        pass
